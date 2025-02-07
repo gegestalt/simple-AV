3ce888766d8ac15b6877b1aaa92f570fbf8af233
gegestalt <guneyunal@sabanciuniv.edu>
2025-02-07 22:07:36 +0300

    updated with the original version. tests will be concluded on another machine.

---
+++ simple-af-av.c
@@ -0,0 +1,39 @@
+
+#include "simple-af-av.h"
+
+Database mkdatabase(int16 cap) {
+	Database db;
+	Entry* p; 
+	int16 size; 
+	db.num = 0; 
+	db.cap = cap; 
+	size = cap * sizeof(Entry);
+	p = (Entry*)malloc($i size);
+	assert(p);
+	zero($c p, size);
+	db.entries = p;
+	return db;
+
+}
+
+void destroydb(Database db) {
+	db.cap = 0;
+	db.num = 0; 
+	free(db.entries);
+
+	return;
+}
+
+void showdb(Database db) {
+	printf("cap:\t%d\nnum:\t%d\n", db.cap, db.num);
+	return;
+}
+
+int main(int argc ,char *argv[]) {
+	Database db; 
+	db = mkdatabase(50000);
+	showdb(db);
+	destroydb(db);
+
+	return 0;
+}
---
+++ simple-af-av.h
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+typedef unsigned long long int int64;
+
+#define $8 (int8 *)
+#define $6 (int16)
+#define $2 (int32)
+#define $4 (int64)
+#define $c (char * )
+#define $i (int)
+
+
+typedef int8 Dir[64];
+typedef int8 File[32];
+
+struct s_entry {
+	Dir* dir; 
+	File* file; 
+};
+
+typedef struct s_entry Entry; 
+
+struct s_database {
+	Entry* entries;
+	int16 cap;
+	int16 num;
+
+};
+
+typedef struct s_database Database;
+
+Database mkdatabase(int16);
+void addtodb(Database, Entry);
+void destroydb(Database);
+void showdb(Database);
+int main(int, char**);
+
