e09c06017cf744e9c8492be39b71ab73653227a5
gegestalt <guneyunal@sabanciuniv.edu>
2025-02-08 11:37:42 +0300

    file addition and successfull compilation added readjusted some errors.

--- simple-af-av.c
+++ simple-af-av.c
@@ -1,13 +1,13 @@
 #include "simple-af-av.h"
 
-Database mkdatabase(int16 cap) {
+Database mkdatabase(int32 cap) {
     Database db;
     Entry* p; 
-    int16 size; 
+    int32 size; 
     
     db.num = 0; 
     db.cap = cap; 
-    size = cap * sizeof(Entry);
+    size = cap * sizeof(struct s_entry);
     p = (Entry*)malloc(size);
     assert(p);
     memset(p, 0, size);  // Replace zero() with memset()
@@ -22,12 +22,46 @@ void destroydb(Database db) {
 }
 
 void showdb(Database db) {
+	int32 n;
+
     printf("cap:\t%d\nnum:\t%d\n", db.cap, db.num);
+	for (n = 0; n < db.num; n++) {
+		printf("Entry %d: %s/%s\n", n, db.entries[n].dir, db.entries[n].file);
+	}
 }
 
+void addto_database (Database db, Entry e) {
+	int32 size, cap, ix;
+
+	//check if the capacity is full
+	if (db.num == db.cap) {
+		cap	= db.cap + Blocksize;
+		size =cap * sizeof(struct s_entry);
+		db.entries = realloc(db.entries, size); //if realloc fails, db.entries will be NULL
+		assert(db.entries);
+		db.cap = cap;
+	}
+	ix = db.num;
+	memcpy($c &e , $c &db.entries[ix],sizeof(struct s_entry));
+	db.num++;
+
+	return;
+
+	
+
+}
 int main(int argc, char *argv[]) {
     Database db; 
+
+	Entry e1, e2;
+	memcpy(e1.file, "FILE1.txt", strlen("FILE1.txt") + 1);
+    memcpy(e1.dir, "/tmp", strlen("/tmp") + 1);
+    memcpy(e2.file, "FILE2.txt", strlen("FILE2.txt") + 1);
+    memcpy(e2.dir, "/tmp", strlen("/tmp") + 1);
+
     db = mkdatabase(50000);
+	addto_database(db, e1);
+	addto_database(db, e2);
     showdb(db);
     destroydb(db);
     return 0;
--- simple-af-av.h
+++ simple-af-av.h
@@ -15,26 +15,34 @@ typedef unsigned short int int16;
 typedef unsigned int int32;
 typedef unsigned long long int int64;
 
-typedef int8 Dir[64];
-typedef int8 File[32];
+#define Blocksize 50000
+#define $1 (int8 * )
+#define $2 (int16)
+#define $4 (int32)
+#define $8 (int64)
+#define $c (char *)
+#define $i (int)
+
+typedef char Dir[64];
+typedef char File[32];
 
 struct s_entry {
-    Dir* dir; 
-    File* file; 
+    Dir dir; 
+    File  file; 
 };
 
 typedef struct s_entry Entry; 
 
 struct s_database {
     Entry* entries;
-    int16 cap;
-    int16 num;
+    int32 cap;
+    int32 num;
 };
 
 typedef struct s_database Database;
 
-Database mkdatabase(int16);
-void addtodb(Database, Entry);
+Database mkdatabase(int32);
+void addto_database(Database, Entry);
 void destroydb(Database);
 void showdb(Database);
 
